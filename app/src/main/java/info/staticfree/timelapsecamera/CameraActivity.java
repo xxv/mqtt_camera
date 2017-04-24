4b9e9dd10a404632815cf4da691135b795693399
Steve Pomeroy <steve@staticfree.info>
2017-04-23 21:10:38 -0400

    Initial commit

---
+++ app/src/main/java/info/staticfree/timelapsecamera/CameraActivity.java
@@ -0,0 +1,96 @@
+package info.staticfree.timelapsecamera;
+
+import android.app.Fragment;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class CameraActivity extends AppCompatActivity {
+    private ListView leftDrawer;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        leftDrawer = (ListView) findViewById(R.id.left_drawer);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open,
+                R.string.drawer_close) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                //getActionBar().setTitle(R.string.app_name);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        leftDrawer.setOnItemClickListener(new DrawerItemClickListener());
+
+        drawerLayout.addDrawerListener(drawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        String[] items = getResources().getStringArray(R.array.drawer_options);
+        leftDrawer.setAdapter(
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items));
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return drawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+
+    private class DrawerItemClickListener implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            if (position == 0) {
+                showCamera();
+            } else if (position == 1) {
+                showSettings();
+            }
+        }
+    }
+
+    private void showSettings() {
+
+    }
+
+    private void showCamera() {
+        Fragment fragment =
+                getFragmentManager().findFragmentByTag(CameraFragment.class.getName());
+
+        if (fragment == null) {
+            fragment = new CameraFragment();
+        }
+
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fragment_content, fragment, CameraFragment.class.getName())
+                .commit();
+    }
+}
